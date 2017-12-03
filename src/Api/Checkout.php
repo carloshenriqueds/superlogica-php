9a09301da568c05a0881410ed861cb2287d16a2f
Carlos Henrique <carlossantosdm@gmail.com>
2017-12-03 17:33:04 -0200

    Enviando serviço de checkout

---
+++ src/Api/Checkout.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SuperLogica\Api;
+use SuperLogica\Route;
+use SuperLogica\ClientInterface;
+
+class Checkout
+{
+    /**
+     * @var string
+     */
+    const ROUT_CHECKOUT = '/financeiro/checkout';
+
+    /**
+     * @var ClientInterface
+     */
+    private $client;
+
+    /**
+     * Service constructor.
+     * @param ClientInterface $client
+     */
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Envia um audio para um número destino
+     * @param array $data
+     * @return mixed
+     */
+    public function enviar($data)
+    {
+        return $this->client->post(
+            new Route([self::ROTA_AUDIO]), [$data]);
+    }
+}

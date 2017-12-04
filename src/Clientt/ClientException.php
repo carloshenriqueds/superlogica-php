8d5baf07637d43189c721238fce2cad8f3b04e48
Carlos Henrique <carlossantosdm@gmail.com>
2017-12-04 17:05:14 -0200

    refactor

---
+++ src/Clientt/ClientException.php
@@ -0,0 +1,4 @@
+<?php
+namespace Client\SuperLogica;
+
+class ClientException extends \Exception {}
---
+++ src/Routee/RouteInterface.php
@@ -0,0 +1,13 @@
+<?php
+namespace Route\SuperLogica;
+
+/**
+ * RouteInterface
+ */
+interface RouteInterface
+{
+    /**
+     * return string
+     */
+    public function build();
+}
